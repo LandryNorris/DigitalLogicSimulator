a7ff1570c02822ab3e62f74da2839c1bfa25ec86
LandryNorris <landry.norris@live.com>
2024-08-03 08:15:39 -0500

    Move single state apply to SingleOutput

--- engine/src/commonMain/kotlin/com/landry/digital/engine/component/LogicGate.kt
+++ engine/src/commonMain/kotlin/com/landry/digital/engine/component/LogicGate.kt
@@ -3,12 +3,7 @@ package com.landry.digital.engine.component
 interface LogicGate {
     val inputs: MutableList<Pin>
     val outputs: List<Pin>
-    var nextState: Boolean
 
     fun update()
-    fun apply() {
-        outputs.forEach {
-            it.state = nextState
-        }
-    }
+    fun apply()
 }
--- engine/src/commonMain/kotlin/com/landry/digital/engine/component/SingleOutput.kt
+++ engine/src/commonMain/kotlin/com/landry/digital/engine/component/SingleOutput.kt
@@ -1,6 +1,13 @@
 package com.landry.digital.engine.component
 
 interface SingleOutput: LogicGate {
+    var nextState: Boolean
     val output: Pin
         get() = outputs.first()
+
+    override fun apply() {
+        outputs.forEach {
+            it.state = nextState
+        }
+    }
 }
