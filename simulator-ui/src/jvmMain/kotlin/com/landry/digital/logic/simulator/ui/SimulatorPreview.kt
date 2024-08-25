08f96893f687935977b852282e99bfd2d6a380e6
LandryNorris <landry.norris@live.com>
2024-08-24 20:24:25 -0500

    Reimplement simulator preview circuit

--- simulator-ui/src/jvmMain/kotlin/com/landry/digital/logic/simulator/ui/SimulatorPreview.kt
+++ simulator-ui/src/jvmMain/kotlin/com/landry/digital/logic/simulator/ui/SimulatorPreview.kt
@@ -6,10 +6,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.landry.digital.engine.component.AndGate
 import com.landry.digital.engine.component.OrGate
-import com.landry.digital.engine.ui.UICircuit
+import com.landry.digital.engine.component.defaultSize
+import com.landry.digital.engine.ui.*
 
 @Preview
 @Composable
 fun SimulatorPreview() {
-
+    val circuit = UICircuit(
+        listOfNotNull(
+            AndGate().apply {
+                input1.state = true
+                input2.state = true
+                output.state = true
+                gateProperties = GateUIProperties(Position(2, 1), AndGate.defaultSize)
+            }.getUIState(),
+            OrGate().apply {
+                input1.state = true
+                input2.state = false
+                output.state = true
+                gateProperties = GateUIProperties(Position(8, 2), AndGate.defaultSize)
+            }.getUIState()
+        ),
+        listOf(
+            WireUIState(
+                positions = listOf(Position(6, 3), Position(8, 3)),
+                true
+            )
+        )
+    )
+    SimulatorLayout(modifier = Modifier.fillMaxSize(), circuit = circuit)
 }
